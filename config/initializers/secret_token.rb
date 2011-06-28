8bf86ab84a0e360ebe05478d9c4476b6e21e5192
ziro sas <ziro@zmac.dummy.porta.siemens.net>
2011-06-28 13:45:36 +1200

    initial_ipmort

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labs::Application.config.secret_token = '616cb3ff30e5d3f4dc5b2b7add4f591ba247b61de90b7f35e999872dcd99ac73a8cecf396f9c3a4ac992963ba1aebfcf09e32a2911f8a12bef09d24ef576eec0'
